04a7b78da9975dd0fd27c1186c3fffc332d5c3fe
SudoJoseph <sudojoseph@pop-os.localdomain>
2022-06-19 17:47:01 +0300

    milestone 1 done

---
+++ index.js
@@ -0,0 +1,4 @@
+const IndexNumber = 6;
+const fibonacciOutput = 8; 
+const outputElm = document.getElementById('output');
+outputElm.innerText = `The Fibonacci of ${IndexNumber} is ${fibonacciOutput}`;
