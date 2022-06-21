8b61cb6f93d6104a202f922eb238a38e7fa287b9
SudoJoseph <sudojoseph@pop-os.localdomain>
2022-06-21 08:17:00 +0300

    milestone 3 done

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-const indexNr = 6;
+//Element selectors
 const outputElm = document.getElementById('output');
+const formElm = document.getElementsByTagName('form')[0];
+const inputElm = document.getElementsByTagName('input')[0];
 
+//Fibonacci Function
 const fibonacciCalculator = index => {
     let prevNr = 0, newNr = 0, outputNr = 0;
 
-    for (let i = 0; i < index; i++) {
-        if (i < 1) {
-            prevNr = 1;
-        }
+    for (let i = 0; i <= index; i++) {
+
+        if (i === 1) prevNr = 1, outputNr = 1;
 
         outputNr = prevNr + newNr;
         prevNr = newNr;
         newNr = outputNr;
     }
 
-    outputElm.innerText = `The Fibonacci of ${index} is ${outputNr}`;
+    outputElm.innerText = outputNr;
 }
 
-fibonacciCalculator(indexNr);
+//Form event handler
+formElm.addEventListener('submit', e => {
+    e.preventDefault();
+    fibonacciCalculator(inputElm.value);
+});
