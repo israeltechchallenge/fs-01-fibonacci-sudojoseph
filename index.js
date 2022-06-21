560ef19aebb9ba36428792306088e40db0cf6d97
sudojoseph <josephdavidson1989@gmail.com>
2022-06-21 08:30:27 +0300

    milestone 3.1 done

--- index.js
+++ index.js
@@ -3,23 +3,27 @@ const outputElm = document.getElementById('output');
 const formElm = document.getElementsByTagName('form')[0];
 const inputElm = document.getElementsByTagName('input')[0];
 
-//Fibonacci Function
+//Counter values for fibonacciCalculator
+let prevNr = 0, newNr = 0, outputNr = 0;
+
+//Fibonacci Calculator function
 const fibonacciCalculator = index => {
-    let prevNr = 0, newNr = 0, outputNr = 0;
-
-    for (let i = 0; i <= index; i++) {
-
-        if (i === 1) prevNr = 1, outputNr = 1;
-
-        outputNr = prevNr + newNr;
-        prevNr = newNr;
-        newNr = outputNr;
+    if (index == 0) {
+        outputElm.innerText = outputNr;
+        prevNr = 0, newNr = 0, outputNr = 0;
+        return;
     }
 
-    outputElm.innerText = outputNr;
+    outputNr = prevNr + newNr;
+    prevNr = newNr;
+    newNr = outputNr;
+
+    if (prevNr === 0) prevNr = 1, outputNr = 1;
+    
+    fibonacciCalculator(index-1);
 }
 
-//Form event handler
+//Event listener
 formElm.addEventListener('submit', e => {
     e.preventDefault();
     fibonacciCalculator(inputElm.value);
