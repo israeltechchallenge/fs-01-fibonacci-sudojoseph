936f1c5e52803681b753d69ffbe4212a506116fd
sudojoseph <josephdavidson1989@gmail.com>
2022-06-21 13:03:00 +0300

    milestone 4 done

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 //Element selectors
-const outputElm = document.getElementById('output');
 const formElm = document.getElementsByTagName('form')[0];
 const inputElm = document.getElementsByTagName('input')[0];
-
-//Counter values for fibonacciCalculator
-let prevNr = 0, newNr = 0, outputNr = 0;
+const outputElm = document.getElementById('output');
 
 //Fibonacci Calculator function
-const fibonacciCalculator = index => {
-    if (index == 0) {
-        outputElm.innerText = outputNr;
-        prevNr = 0, newNr = 0, outputNr = 0;
-        return;
-    }
-
-    outputNr = prevNr + newNr;
-    prevNr = newNr;
-    newNr = outputNr;
-
-    if (prevNr === 0) prevNr = 1, outputNr = 1;
-    
-    fibonacciCalculator(index-1);
+const fibonacciCalculator = number => {
+    fetch(`http://localhost:5050/fibonacci/${number}`)
+    .then(response => response.json())
+    .then(data => outputElm.innerText = data.result);
 }
 
 //Event listener
