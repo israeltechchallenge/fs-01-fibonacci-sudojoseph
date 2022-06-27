f39016f829c9f4e642181092cc9a291d294e7576
sudojoseph <josephdavidson1989@gmail.com>
2022-06-27 08:59:34 +0300

    milestone 6 done

--- index.js
+++ index.js
@@ -2,48 +2,89 @@
 const formElm = document.getElementsByTagName('form')[0];
 const inputElm = document.getElementsByTagName('input')[0];
 const outputElm = document.getElementById('output');
-const spinnerElm = document.getElementById('spinner');
+const specificNrSpinner = document.getElementById('spinnerSpecificNr');
+const spinnerAllCalculations = document.getElementById('spinnerAllCalculations');
 const serverError = document.getElementById('serverError');
 const lessThanFiftyErr = document.getElementById('lessThanFiftyErr');
+const allCalculationsList = document.getElementById('allCalculationsList');
 
 
-//Fibonacci Calculator function
-const fibonacciCalculator = number => {
+//Fibonacci Calculator 
+const showSpecificFibonacciNr = number => {
     if (!number) return;
+    const URL = `http://localhost:5050/fibonacci/${number}`;
     
     //Initialize elements
     showValidateMsg(false);
     serverError.innerText = '';
     outputElm.innerText = '';
 
+    //Callback functions for success and error
+    const onSuccess = data => {
+        outputElm.innerText = data.result;
+        toggleSpinner(specificNrSpinner);
+        showAllCalculations();
+    };
+
+    const onError = errMsg => {
+        serverError.innerText = errMsg;
+        toggleSpinner(specificNrSpinner);
+    };
+
+
+
     //Fronted validation check
     if (number <= 50) {
-        toggleSpinner();
-        apiHandeling(number)
+        toggleSpinner(specificNrSpinner);
+        apiHandeling(URL, onSuccess, onError)
     } else {
         showValidateMsg(true);
     }
 }
 
-const apiHandeling = async num => { 
-    const response = await fetch(`http://localhost:5050/fibonacci/${num}`);
+const showAllCalculations = () => {
+    const URL = 'http://localhost:5050/getFibonacciResults';
+
+    toggleSpinner(spinnerAllCalculations);
+
+    const onSuccess = data => {
+        let elmList = '';
+
+        data.results.map(calcObj => elmList += `<li class="border-bottom border-dark pb-3 pt-3">The Fibonacci of <b>${calcObj.number}</b> is <b>${calcObj.result}</b>. Calculated at: ${new Date(calcObj.createdDate)}</li>`);
+
+        allCalculationsList.innerHTML = elmList;
+        toggleSpinner(spinnerAllCalculations);
+
+        // console.log(data.results);
+    };
+
+    const onError = () => {
+        toggleSpinner(spinnerAllCalculations);
+    };
+
+    apiHandeling(URL, onSuccess);
+}
+
+//Api fetch function
+
+const apiHandeling = async (url, onSuccess, onError) => { 
+    const response = await fetch(url);
 
     if(!response.ok) {
-        await response.text().then(errorText => serverError.innerText = errorText);
-        toggleSpinner();
+        const errorText = await response.text()
+        onError(errorText);
         throw new Error(`¯¯\\_(ツ)_//¯¯ Noooo you got an error with the following status: ${response.status}`);
     }
 
     const data = await response.json();
-    outputElm.innerText = data.result;
-    toggleSpinner();
+    onSuccess(data);
 }
 
-const toggleSpinner = () => {
-    if (!spinnerElm.classList.contains('d-none')) {
-        spinnerElm.classList.add('d-none');
+const toggleSpinner = spinner => {
+    if (!spinner.classList.contains('d-none')) {
+        spinner.classList.add('d-none');
     } else {
-        spinnerElm.classList.remove('d-none');
+        spinner.classList.remove('d-none');
     }
 } 
 
@@ -60,5 +101,7 @@ const showValidateMsg = (show) => {
 //Event listener
 formElm.addEventListener('submit', e => {
     e.preventDefault();
-    fibonacciCalculator(inputElm.value);
+    showSpecificFibonacciNr(inputElm.value);
 }); 
+
+showAllCalculations();
